113059d2e06e9d3dba5c1e5c503f4704757e92e0
nidchanan_w <nidchanan_w@NIDCHANAN-W>
2026-03-16 21:11:43 +0700

    app desktop

---
+++ FlamingoShopDemo/wwwroot/js/sw.js
@@ -0,0 +1,35 @@
+﻿const CACHE_NAME = "flamingo-cache-v1";
+
+const urlsToCache = [
+    "/",
+    "/css/site.css",
+    "/css/login.css",
+    "/css/customer.css",
+
+    "/js/site.js",
+    "/js/admin.js",
+    "/js/login.js",
+    "/js/user.js",
+    "/js/order.js",
+    "/js/product.js",
+
+    "/favicon.ico"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
